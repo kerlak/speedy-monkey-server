cd79503228b34bbafe57d0cfb4cd62ccd9cfea26
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2016-06-13 12:45:46 +0200

    fix creation_time precision

--- server/cyclone_server.py
+++ server/cyclone_server.py
@@ -5,35 +5,35 @@ import sys
 from cassandra.cluster import Cluster
 from twisted.internet import reactor
 from twisted.python import log
-from datetime import datetime
+import time
 
+def getMilliseconds():
+    return int(time.time()*1000)
 
-def insert_to_cassandra(count_id, session):
+def insert_to_cassandra(self):
     cql = "INSERT INTO http_request_log.http_request_time \
            (creation_time, host, id, insertion_time) VALUES \
-           ('%s',0,%s,toTimestamp(now()))" % (datetime.now().strftime('%s'), count_id)
-    session.execute_async(cql)
+           ('%s',0,777,toTimestamp(now()))" % (getMilliseconds())
+    self.cassandra_session.execute_async(cql)
+
 
 
 class MainHandler(cyclone.web.RequestHandler):
-    def initialize(self, count_id, db_session):
-        self.count_id = count_id
-        self.db_session = db_session
+    def initialize(self, db_session):
+	self.cassandra_session = db_session
     def get(self):
         self.write("Hello, world")
     def post(self):
-        self.count_id += 1
-        insert_to_cassandra(self.count_id, self.db_session)
+        insert_to_cassandra(self)
 
 
 if __name__ == "__main__":
 
-    count_id = 0
     cluster = Cluster(['reto3db'])
     session = cluster.connect('http_request_log')
 
     application = cyclone.web.Application([
-        (r"/", MainHandler, dict(count_id=count_id, db_session=session))
+        (r"/", MainHandler, dict(db_session=session))
     ])
 
     log.startLogging(sys.stdout)
